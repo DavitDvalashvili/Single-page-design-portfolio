67dae4de98e8b8379741f4b376229d8d16e5a157
“davitdvalashvili <>
2023-02-12 00:38:03 +0400

    finish

---
+++ script.js
@@ -0,0 +1,30 @@
+const slider = document.getElementsByClassName("slider")[0];
+const sliderImg = document.querySelectorAll(".slider img");
+// buttons
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
+let counter = 0;
+let size = sliderImg[0].clientWidth;
+let gap = parseInt(getComputedStyle(slider).gap);
+
+
+// slider.style.transform = `translateX(${-size*counter}px)`;
+
+leftArrow.addEventListener("click",  () => {
+    if (counter == 2) {
+        return 
+    } else {
+        counter ++;
+        slider.style.transform = `translateX(${-size*counter-gap*counter}px)`;
+    }
+})
+
+rightArrow.addEventListener("click",  () => {
+    if (counter == -2) {
+        return 
+    } else {
+        counter --;
+        slider.style.transform = `translateX(${-size*counter-gap*counter}px)`;
+    }
+})
